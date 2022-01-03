0b2c7b6b0682df6e2aee660d74b3b74a8032ea96
Michael Noon <m.noon83@yahoo.com>
2022-01-02 21:10:20 -0800

    install nodemon

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express=require('express')
+const {join}=require('path')
 const app=express()
-app.get('/test', (req, res)=>{
-  console.log(req)
-  console.log(res)
+app.get('/',(req,res)=>{
+  res.sendFile(join(__dirname,'login.html'))
 })
+// app.get('/test', (req, res)=>{
+//   res.send()
+// })
 app.listen(8080)
