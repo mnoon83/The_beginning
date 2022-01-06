4804387d5b5de2d9521c082a454cb2f9d1b7e0a8
Michael Noon <m.noon83@yahoo.com>
2022-01-06 00:59:03 -0800

    square one

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express=require('express')
 const {join}=require('path')
 const app=express()
-app.get('/',(req,res)=>{
-  res.sendFile(join(__dirname,'login.html'))
-})
+app.use(express.static(join(__dirname, 'public')))
+// app.get('/',(req,res)=>{
+//   res.sendFile(join(__dirname,'login.html'))
+// })
 // app.get('/test', (req, res)=>{
 //   res.send()
 // })
