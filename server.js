1b70fb4d33ce0e7c844b3eb79db647fbd543ab05
Michael Noon <m.noon83@yahoo.com>
2022-02-23 23:18:40 -0800

    code to server

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-const exp = require('constants')
-const express=require('express')
-const {join}=require('path')
+const express = require('express')
+const {join} = require('path')
+const passport = require('passport')
+const { Strategy: JWTStrategy, ExtractJWT } = require('passport-jwt')
+
+
 const app=express()
+
 app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({exrended:true}))
 app.use(express.json())
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use()
 
 app.listen(process.env.PORT || 3000)
